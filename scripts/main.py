695a8f75348f9b6c61c58aa7dbb83c76a0b5275f
treemorse <dzondberg@gmail.com>
2022-10-02 20:04:57 +0300

    parser finally working like it supposed to

--- scripts/main.py
+++ scripts/main.py
@@ -19,6 +19,9 @@ options.add_argument('--start-maximized')
 options.add_argument('--disable-extensions')
 
 class DataModel:
+
+    out_of_stock = False
+
     def __init__(
             self, full_url="", 
             product_name="", description="", 
@@ -37,15 +40,29 @@ class DataModel:
         self.chars = characteristics
 
     def __str__(self):
-        return f'Name: {self.name}\nPrice: {self.price}\nRating: {self.rating}\n\n'
+        return f'Name: {self.name}\nPrice: {self.price}\nRating: \
+        {self.rating}\nNumber of reviews: {self.numrev}\nDescription: \
+        {self.desc}\nCharacteristics: {self.chars}\nShop: {self.shop} \
+        \n\nOut of stock: {self.out_of_stock}'
 
     def put_in_csv(self):
         pass
 
 
-def parse(chrome):
+def parse(chrome, product_url):
     model = DataModel()
+
+    if WebDriverWait(chrome, 1).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '/html/body/div[1]/div/div[1]/div[3]/div[1]/div/div[3]/h2'
+                )
+            )).text.replace(" ", "") == "Этоттоварзакончился":
+            model.out_of_stock = True
     try:
+        model.url = product_url
+
         model.name = WebDriverWait(chrome, 1).until(
             EC.presence_of_element_located(
                 (
@@ -53,6 +70,7 @@ def parse(chrome):
                     '//*[@id="layoutPage"]/div[1]/div[3]/div[2]/div/div/div[1]/div/h1'
                 )
             )).text
+        
         model.price = WebDriverWait(chrome, 1).until(
             EC.presence_of_element_located(
                 (
@@ -72,8 +90,57 @@ def parse(chrome):
                 )).get_attribute("style").split()[1].replace(';', '')
         except:
             model.rating = "No rating"
+        try: 
+            model.shop = WebDriverWait(chrome, 3).until(
+                EC.presence_of_element_located(
+                    (
+                        By.XPATH,
+                        '/html/body/div[1]/div/div[1]/div[5]/div/div[1]/ \
+                        div[2]/div/div[1]/div/div[1]/div[1]/div/div[2]/div[1]'
+                    )
+                )).text
+        except:
+            model.shop = WebDriverWait(chrome, 3).until(
+                EC.presence_of_element_located(
+                    (
+                        By.XPATH,
+                        '/html/body/div[1]/div/div[1]/div[5]/div/div[1]/ \
+                        div[2]/div/div[1]/div/div[1]/div[1]/div/div/div[1]/div[2]/a'
+                    )
+                )).text
+        
+        try: 
+            model.desc = WebDriverWait(chrome,3).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//*[@id="section-description"]'
+                )
+            )).text
+        except:
+            model.desc = "No description"
+        
+        try: 
+            model.chars = WebDriverWait(chrome, 3).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '//*[@id="section-characteristics"]'
+                )
+            )).text
+        except:
+            model.char = "No characteristics"
+        
+        model.numrev = WebDriverWait(chrome, 3).until(
+            EC.presence_of_element_located(
+                (
+                    By.XPATH,
+                    '/html/body/div[1]/div/div[1]/div[5]/div/div[1]/div[3]/div[3]/div/div[2]/div/div'
+                )
+            )).text
+
     except Exception as e:
-        print(e)
+        print(model)
         return 1
     print(model)
     return 0
@@ -87,8 +154,9 @@ def excel_to_csv(file):
 def open_window(row):
     driver = webdriver.Chrome(os.environ.get("CHROME_PATH"), options=options)
     driver.maximize_window()
-    driver.get(BASE_URL + row['ID'] + END_URL)
-    if parse(driver):
+    product_url = BASE_URL + row['ID'] + END_URL
+    driver.get(product_url)
+    if parse(driver, product_url):
         open_window(row)
     driver.close()
 
@@ -103,6 +171,7 @@ def main():
         for row in reader:
             count += 1
             open_window(row)
+            sleep(15)
             print(count)
 
     print(count)
